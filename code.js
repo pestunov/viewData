a7ac1c63a63bc0f02c4ed16b9bb9a66e97c7c5fe
pestunov <pest@iao.ru>
2020-10-30 14:21:22 +0700

    Update code.js
    parse schema from schemaJSON.js, add some style features to stylish charts according to schema

--- code.js
+++ code.js
@@ -2,19 +2,18 @@ function obnovit_stranicu() {
 //	location.reload();
 }
 
-
 /****** program here ******/
 //document.body.onload = addElement;
 var schemaParsed = JSON.parse(JSON.stringify(schema));
 var ncont = document.getElementById("empty");
-for (i in schemaParsed){
+for (ii in schemaParsed){
+	var schemaBlock = schemaParsed[ii];
 	var graphBlock = document.createElement("div");
-	graphBlock.className = "dcont"
-	graphBlock.style.width = schemaParsed[i]["width"];
+	graphBlock.style.width = schemaParsed[ii]["width"];
 	graphBlock.style.height = "300px";
 	ncont.append(graphBlock);
 	var canva = document.createElement("canvas");
-	canva.id = "graph"+i;
+	canva.id = "graph"+ii;
 	wid = graphBlock.clientWidth; hei =  graphBlock.clientHeight;
 	canva.width = wid;
 	canva.height = hei;
@@ -22,40 +21,25 @@ for (i in schemaParsed){
 		/* draw graph */
 	var ctx=canva.getContext("2d");
 	var chart=new MyChart(ctx, wid, hei);
+	//to do: find min and max values in all lists assosiated with this graph
+	var graphBlock = schemaBlock["graph"];
+
+	yCaption = "";
+	for (jj in graphBlock){
+		yCaption = yCaption + graphBlock[jj]["name"] + " ";
+	}
+
 	
-	chart.drawGridDate(DateTimeSec,Data1, "Datetime", "CO2air");
-	chart.drawChartDot(DateTimeSec, Data1, 3, 'lightgreen');
+	if (schemaParsed[ii]["type"] == "datetime") {
+		chart.drawGridDate(data[0], data[schemaParsed[ii]["graph"][0]["y"]], "Datetime", yCaption);
+	} else {
+		chart.drawGrid(data[0], data[2], "Datetime", yCaption);
+	}
+	for (jj in graphBlock){
+		var xi = graphBlock[jj]["x"]; var yi = graphBlock[jj]["y"];
+		chart.drawChartDot(data[xi], data[yi], 2, graphBlock[jj]["color"]);
+	}
 }
-
-
-
-
-var winWidth = document.documentElement.clientWidth;
-
-var c=document.getElementById("gri1");
-var dw=c.clientWidth; var dh=c.offsetHeight-6;
-var c=document.getElementById("chart11");
-var ctx=c.getContext("2d");
-c.width=dw; c.height=dh;
-console.log(c.width, c.height);
-var chart1=new MyChart(ctx, c.width, c.height);
-chart1.drawGridDate(DateTimeSec,Data1, "Datetime", "CO2air");
-chart1.drawChartDot(DateTimeSec, Data1, 3, 'lightgreen');
-
-var c=document.getElementById("gri2");
-var dw=c.clientWidth; var dh=c.offsetHeight-6;
-var c=document.getElementById("chart12");
-var ctx=c.getContext("2d");
-c.width=dw; c.height=dh;
-var chart1 = new MyChart(ctx, c.width, c.height);
-chart1.drawGridDate(DateTimeSec, Data3, "DateTime", "CO2bot_sur");
-chart1.drawChartPolygon(DateTimeSec,Data2,Data3, 'rgba(225,10,0,0.2)');
-chart1.drawChartLine(DateTimeSec, Data3, 1, 'darkgreen');
-chart1.drawChartLine(DateTimeSec, Data2, 1, 'darkgreen');
-
-//chart3.drawGridDate(DateTimeSec, Data3, "Datetime", "CO2Bot");
-//chart3.drawChartDot(DateTimeSec, Data3, 2, 'magenta');
-
 
 
 var timer_reload = setInterval("obnovit_stranicu()", 60000);
